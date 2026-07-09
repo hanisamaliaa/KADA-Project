62100f42abab68a63db9831fa082ed2833556813
EevnxyEgo <arseniuswahyu@gmail.com>
2026-07-10 01:05:00 +0700

    Enhance booking functionality with improved validation and error handling

--- Cinema-Booking-Backend/src/routes/bookingRoutes.js
+++ Cinema-Booking-Backend/src/routes/bookingRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const Booking = require("../models/Booking");
-const requireAdmin = require("../middlewares/requireAdmin");
 const authenticate = require("../middlewares/authenticate");
 
 
 
-const { createBooking } = require("../controllers/bookingController");
+const { createBooking, getMyBookings, getBookingById, cancelBooking } =
+  require("../controllers/bookingController");
 
 router.post("/", authenticate, createBooking);
-
+router.get("/me", authenticate, getMyBookings);       // ← SEBELUM /:id
+router.get("/:id", authenticate, getBookingById);
+router.delete("/:id", authenticate, cancelBooking);
 
 module.exports = router;
